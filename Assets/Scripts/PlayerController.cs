856ae0fa05a236b709ecbbfbdfd5bf40f07b4f8f
devanpverma <devanpverma@gmail.com>
2025-07-06 13:39:28 -0400

    Add: rigid bodies to game objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            }
 }
